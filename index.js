fbff8ae6fc7d9a008c639f84414395ddc698aeed
Himadri <himadris@gmail.com>
2020-06-04 18:48:44 -0700

    updates

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-console.log ("123");
+
+const inquirer = require('inquirer');
+const create = require ("./createReadme");
+
+
+function getUserInput() {
+	return inquirer.prompt([
+		{
+			type: "input",
+			message: "Please enter the title of your repository",
+			name: "title",
+        },
+        {
+            type: "input",
+            message:"Please enter the description of your Repository",
+            name:"description"
+        },
+        {
+            type: "input",
+            message: "Please list a table of Contents",
+            name: "Contents",
+        },
+        {
+            type: "input",
+            message: "How do you install this program?",
+            name: "Installation",
+        },
+        {
+            type: "input",
+            message: "Please enter a license if any",
+            name: "License",
+        },
+        {
+            type: "input",
+            message: "Please list any contributors",
+            name: "Contributors",
+        },
+        {
+            type: "input",
+            message: "Do you have a test for this application?",
+            name: "Tests",
+        },
+        {
+            type: "input",
+            messages: "Any questions?",
+            name: "Questions",
+    },
+		{
+            type: "input",
+            messages:"What is your github username?",
+            name:"Github",
+
+        },
+
+	])
+}
+
+
+
+async function main() {
+	const { username } = await getUserInput();
+
+}
+
+main();
+
+
---
+++ index.md
@@ -0,0 +1,29 @@
+Please Enter The Title of Your Repository
+$[title]
+
+Please Enter the Description of Your Repository
+$[description]
+
+Please list a table of contents
+$[Contents]
+
+How do you install this program
+$[Installation]
+
+Please enter a license if any
+$[License]
+
+Please list any contributors
+$[Contributors]
+
+Do you hava a test for this application?
+$[Tests]
+
+Do you have any questions?
+$[Questions]
+
+What is your github username?
+$[Github]
+
+
+
